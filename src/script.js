0e5254c132d48263b970a7b03a1e6666bdeacda0
journeycruz-ping <journeycruz@pingidentity.com>
2023-01-25 14:01:40 -0600

    update

--- src/script.js
+++ src/script.js
@@ -19,10 +19,10 @@ function init() {
     camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.set(5, 0, 0);
     scene.rotation.y = -270 * Math.PI / 180;
-    const ambient = new THREE.AmbientLight(0xffffff, 0);
+    const ambient = new THREE.AmbientLight(0xffffff, 2);
     scene.add(ambient);
 
-    const light = new THREE.DirectionalLight(0xffffff, 0);
+    const light = new THREE.DirectionalLight(0xffffff, 2);
     light.position.set(50, 50, 100);
     scene.add(light);
 
@@ -55,6 +55,35 @@ function init() {
         }
     )
 
+    const loader2 = new THREE.GLTFLoader()
+    loader2.load(
+        'models/bg-boxes-9.glb',
+        function(gltf) {
+            scene.add(gltf.scene)
+
+
+            gsap.registerPlugin(ScrollTrigger);
+
+            gsap.to(gltf.scene.rotation, {
+                scrollTrigger: {
+                    trigger: "#trigger",
+                    start: "+=150px",
+                    end: "+=360px",
+                    markers: false,
+                    scrub: true,
+                    toggleActions: "restart pause resume pause"
+                },
+                y: Math.PI
+            });
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log(error)
+        }
+    )
+
     renderer = new THREE.WebGLRenderer({
         antialias: true,
         alpha: true
@@ -62,12 +91,12 @@ function init() {
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight * 2);
     document.getElementById("c").appendChild(renderer.domElement);
-
     controls = new THREE.OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
     controls.enableZoom = false;
     controls.enablePan = false;
 }
+
 
 function animate() {
     requestAnimationFrame(animate);
